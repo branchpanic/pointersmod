e4233d0c21749c293a33221c269c18e0f1386738
dhsavell05@gmail.com <computersunified@gmail.com>
2016-10-09 13:51:40 -0400

    Update version number in some files

--- src/main/java/notjoe/pointersmod/PointersMod.java
+++ src/main/java/notjoe/pointersmod/PointersMod.java
@@ -23,7 +23,7 @@ import org.apache.logging.log4j.Logger;
      */
     public static final String MODID = "pointersmod";
     public static final String MODNAME = "Pointers";
-    public static final String VERSION = "1.10.2-0.1-alpha.1";
+    public static final String VERSION = "1.10.2-0.2-beta.0";
     public static final String DEPENDENCIES = "required-after:tesla@[1.2.1.49,);";
 
     /**
