9ef70dca3a7699d2d48baec40a2a84edaefed0cc
dhsavell05@gmail.com <computersunified@gmail.com>
2016-10-09 12:33:48 -0400

    Particles for Positional Pointer teleports

--- src/main/java/notjoe/pointersmod/api/actions/PointerActionTeleport.java
+++ src/main/java/notjoe/pointersmod/api/actions/PointerActionTeleport.java
@@ -2,13 +2,17 @@ package notjoe.pointersmod.api.actions;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.world.World;
 import notjoe.pointersmod.api.BlockInWorld;
 import notjoe.pointersmod.api.PointerAction;
 import notjoe.pointersmod.api.helpers.NbtHelper;
 import notjoe.pointersmod.common.Config;
 
+import java.util.Random;
+
 public class PointerActionTeleport extends PointerAction {
+    private static Random random = new Random();
     @Override
     public boolean setPointerTarget(ItemStack stack, BlockInWorld blockInWorld, World world) {
         NbtHelper.initNbtTagForStack(stack);
@@ -24,6 +28,15 @@ public class PointerActionTeleport extends PointerAction {
             BlockInWorld blockInWorld = new BlockInWorld(stack.getTagCompound());
             player.setPosition(blockInWorld.pos.getX() + 0.5, blockInWorld.pos.getY() + 0.5,
                 blockInWorld.pos.getZ() + 0.5);
+            for(int i = 0; i < 20; i++) {
+                world.spawnParticle(EnumParticleTypes.REDSTONE,
+                    player.posX + random.nextGaussian(),
+                    player.posY,
+                    player.posZ + random.nextGaussian(),
+                    random.nextGaussian(),
+                    random.nextGaussian(),
+                    random.nextGaussian());
+            }
             return true;
         }
         return false;
